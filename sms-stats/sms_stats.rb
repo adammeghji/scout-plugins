e9148abb29e1551f90c4be741d0f2cd32baa7b86
Jeff Judge <jeff@interactivemediums.com>
2008-09-28 09:53:39 -0500

    Adding basic SMS stats

---
+++ sms-stats/sms_stats.rb
@@ -0,0 +1,18 @@
+class SmsStats < Scout::Plugin
+  needs 'mysql'
+
+  def build_report
+    user = option(:user) || 'root'
+    password = option(:password)
+    host = option(:host) || 'localhost'
+    port = option(:port) || 3306
+    socket = option(:socket) || '/tmp/mysql.sock'
+    database = option(:database)
+
+    mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
+    results = mysql.query("select sum(if(message_type='MT',1,0)) as mt, sum(if(message_type='MO',1,0)) as mo from message_history where created_at > DATE_SUB(now(), INTERVAL 5 MINUTE) and error_code is null")
+
+    report(:MT => results[0]['mt'].to_i, :MO => results[0]['mo'].to_i)
+  end
+  
+end
