959b8ead17fa9b04d575a033b17ec01a78c4b916
Doug Barth <dougbarth@gmail.com>
2008-09-29 16:05:30 -0500

    Adding average transaction time and average aggregator time.

--- sms-stats/sms_stats.rb
+++ sms-stats/sms_stats.rb
@@ -10,12 +10,19 @@ class SmsStats < Scout::Plugin
     database = option(:database)
 
     mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
-    results = mysql.query("select sum(if(message_type='MT',1,0)) as mt, sum(if(message_type='MO',1,0)) as mo from message_history where created_at > DATE_SUB(now(), INTERVAL 5 MINUTE) and error_code is null")
+    results = mysql.query <<-SQL
+      select sum(if(message_type='MT',1,0)) as mt, sum(if(message_type='MO',1,0)) as mo,
+             avg(transaction_time) as avg_transaction_time,
+             avg(aggregator_time) as avg_aggregator_time
+        from message_history
+       where created_at > DATE_SUB(now(), INTERVAL 5 MINUTE)
+         and error_code is null
+    SQL
 
     results.each_hash do |row|
-     report(:MT => row['mt'].to_i, :MO => row['mo'].to_i)     
+      report(:MT => row['mt'].to_i, :MO => row['mo'].to_i,
+             'Average Transaction Time' => row['avg_transaction_time'],
+             'Average Aggregator Time' => row['avg_aggregator_time'])
     end
-
   end
-  
 end
