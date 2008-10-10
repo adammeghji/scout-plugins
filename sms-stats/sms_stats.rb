cd374f404d48dc55c3d81c063912ad8394dfe9df
Doug Barth <dougbarth@gmail.com>
2008-10-10 08:48:57 -0500

    Track failed MTs and return 0 on no messages.

--- sms-stats/sms_stats.rb
+++ sms-stats/sms_stats.rb
@@ -11,16 +11,17 @@ class SmsStats < Scout::Plugin
 
     mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
     results = mysql.query <<-SQL
-      select sum(if(message_type='MT',1,0)) as mt, sum(if(message_type='MO',1,0)) as mo,
+      select sum(if(message_type='MT' and error_code is null,1,0)) as mt, sum(if(message_type='MO',1,0)) as mo,
+             sum(if(message_type = 'MT' and error_code is not null,1,0)) as failed_mt,
              avg(transaction_time) as avg_transaction_time,
              avg(aggregator_time) as avg_aggregator_time
         from message_history
        where created_at > DATE_SUB(now(), INTERVAL 5 MINUTE)
-         and error_code is null
     SQL
 
     results.each_hash do |row|
-      report(:MT => row['mt'].to_i, :MO => row['mo'].to_i,
+      report(:MT => row['mt'] || 0, :MO => row['mo'] || 0,
+             'Failed MTs' => row['failed_mt'] || 0,
              'Average Transaction Time' => row['avg_transaction_time'],
              'Average Aggregator Time' => row['avg_aggregator_time'])
     end
