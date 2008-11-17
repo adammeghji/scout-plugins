de66dbf08e14f5d4bc31be8f67675b35f53715c1
dave farkas <dave@interactivemediums.com>
2008-11-17 12:52:06 -0600

    set reports and alerts separately

--- reliable-msg-stats/reliable_msg_stats.rb
+++ reliable-msg-stats/reliable_msg_stats.rb
@@ -9,20 +9,18 @@ class ReliableMsgStats < Scout::Plugin
     socket = option(:socket) || '/tmp/mysql.sock'
     database = option(:database)
     max_queue_size = option(:max_queue_size) || 100
-    data = {}
-    alerts = []
 
     mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
     results = mysql.query('SELECT queue, count(1) from reliable_msg_queues group by queue')
 
     report_data = {}
+    alert_data = []
     results.each do |row|
       report_data[row[0]] = row[1].to_i
-      alerts << {:subject => "Maximum Queue Size Exceeded for Queue: #{row[0]} - Size: #{row[1]}"} if row[1].to_i >= max_queue_size.to_i && row[0] != '$dlq'
+      alert_data << {:subject => "Maximum Queue Size Exceeded for Queue: #{row[0]} - Size: #{row[1]}"} if row[1].to_i >= max_queue_size.to_i && row[0] != '$dlq'
     end
 
-    data[:alerts] = alerts unless alerts.empty?
-    data[:report] = report_data
-    data
+    alert(alert_data) unless alert_data.empty?
+    report(report_data)
   end
 end
