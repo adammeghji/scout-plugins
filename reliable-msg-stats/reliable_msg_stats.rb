029959b2b910fb082bfde20b35f3c42672bcf913
dave farkas <dave@interactivemediums.com>
2008-11-17 17:01:34 -0600

    fixing alerts

--- reliable-msg-stats/reliable_msg_stats.rb
+++ reliable-msg-stats/reliable_msg_stats.rb
@@ -14,10 +14,9 @@ class ReliableMsgStats < Scout::Plugin
     results = mysql.query('SELECT queue, count(1) from reliable_msg_queues group by queue')
 
     report_data = {}
-    alerts = []
     results.each do |row|
       report_data[row[0]] = row[1].to_i
-      alerts << {:subject => "Maximum Queue Size Exceeded for Queue: #{row[0]} - Size: #{row[1]}"} if row[0] != '$dlq' && row[1].to_i >= max_queue_size.to_i
+      alert("Maximum Queue Size Exceeded for Queue: #{row[0]} - Size: #{row[1]}") if row[0] != '$dlq' && row[1].to_i >= max_queue_size.to_i
     end
 
     report(report_data)
