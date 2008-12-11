a0eb6b0b4d05459199158e417441014db5f5eb8b
dave farkas <dave@interactivemediums.com>
2008-12-11 12:29:00 -0600

    changed report to get count, max, min, and total memory

--- passenger_rails_memory/passenger_rails_memory.rb
+++ passenger_rails_memory/passenger_rails_memory.rb
@@ -1,7 +1,7 @@
 class PassengerRailsMemory < Scout::Plugin
   def build_report
     cmd  = option(:passenger_memory_stats_command) || "passenger-memory-stats"
-    @rails_instance = option(:rails_instance) || "rails"
+    @rails_instance = option(:rails_instance) || "tmb"
     data = `#{cmd} | grep Rails | grep #{@rails_instance}`.to_a
     stats = parse_data(data)
     report(stats)
@@ -9,12 +9,17 @@ class PassengerRailsMemory < Scout::Plugin
 
   def parse_data(data)
     report_data = {}
+    report_data["count"] = data.size
+    memory = []
     data.each { | instance | 
        fields = instance.split
-       pid = fields[0]
-       memory = fields[2]
-      report_data["#{@rails_instance}_#{pid}"] = memory
+       memory << fields[2].to_f
     }
+
+    report_data["total memory (MB)"] = memory.inject(0){|sum,item| sum + item}
+    report_data["max memory (MB)"] = memory.max
+    report_data["min memory (MB)"] = memory.min
+
     report_data
   end
 end
