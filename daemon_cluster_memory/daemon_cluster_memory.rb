d82829447f55eb641a5b887420cd4678964ae466
dave farkas <dave@interactivemediums.com>
2009-01-23 11:32:25 -0600

    added daemon cluster memory

---
+++ daemon_cluster_memory/daemon_cluster_memory.rb
@@ -0,0 +1,29 @@
+class DaemonClusterMemory < Scout::Plugin
+  def build_report
+    ps_command = @options['ps_command'] || 'ps axucww'
+    ps_output = `#{ps_command}`.to_a
+
+    fields = ps_output.first.downcase.split
+    memory_index = fields.index('rss')
+    pid_index = fields.index('pid')
+
+    pid_dir = @options['pid_dir']
+    unless File.exist?(pid_dir)
+      error("PID directory not found", "#{pid_dir} was not found on the file system")
+      return
+    end
+
+    report_data = {}
+    Dir[File.join(pid_dir, '*.pid')].each do |pid_file|
+      port = pid_file.split('_')[1].split('.')[0]
+      pid = File.read(pid_file)
+
+      ps_line = ps_output.detect {|line| line.split[pid_index] == pid}
+      memory = ps_line ? Float(ps_line.split[memory_index]) / 1024 : nil
+
+      report_data["Process #{port}"] = memory
+    end
+
+    report(report_data)
+  end
+end
