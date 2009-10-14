0848a6a926aa612c61705de2fcf55dba08758272
Doug Barth <dougbarth@gmail.com>
2009-10-14 14:57:33 -0500

    Use previous run timestamp to determine counting interval.

--- sms-stats/sms_stats.rb
+++ sms-stats/sms_stats.rb
@@ -19,30 +19,39 @@
 class SmsStats < Scout::Plugin
   needs 'mysql'
 
+  DB_FORMAT = '%Y-%m-%d %H:%M:%S'
+
   def build_report
-    user = option(:user) || 'root'
-    password = option(:password)
-    host = option(:host) || 'localhost'
-    port = option(:port) || 3306
-    socket = option(:socket) || '/tmp/mysql.sock'
-    database = option(:database)
+    last_run = memory(:last_run)
 
-    mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
-    results = mysql.query <<-SQL
-      select sum(if(message_type='MT' and error_code is null,1,0)) as mt,
-             sum(if(message_type='MO',1,0)) as mo,
-             sum(if(message_type = 'MT' and error_code is not null,1,0)) as failed_mt,
-             avg(transaction_time) as avg_transaction_time,
-             avg(aggregator_time) as avg_aggregator_time
-        from recent_messages
-       where created_at > DATE_SUB(now(), INTERVAL 5 MINUTE)
-    SQL
+    # Will calculate message deltas on the next run.
+    unless last_run.nil?
+      user = option(:user) || 'root'
+      password = option(:password)
+      host = option(:host) || 'localhost'
+      port = option(:port) || 3306
+      socket = option(:socket) || '/tmp/mysql.sock'
+      database = option(:database)
 
-    results.each_hash do |row|
-      report(:MT => row['mt'] || 0, :MO => row['mo'] || 0,
-             'Failed MTs' => row['failed_mt'] || 0,
-             'Average Transaction Time' => row['avg_transaction_time'],
-             'Average Aggregator Time' => row['avg_aggregator_time'])
+      mysql = Mysql.connect(host, user, password, database, port.to_i, socket)
+      results = mysql.query <<-SQL
+        select sum(if(message_type='MT' and error_code is null,1,0)) as mt,
+               sum(if(message_type='MO',1,0)) as mo,
+               sum(if(message_type = 'MT' and error_code is not null,1,0)) as failed_mt,
+               avg(transaction_time) as avg_transaction_time,
+               avg(aggregator_time) as avg_aggregator_time
+          from recent_messages
+         where created_at > #{last_run.strftime(DB_FORMAT)}
+      SQL
+
+      results.each_hash do |row|
+        report(:MT => row['mt'] || 0, :MO => row['mo'] || 0,
+               'Failed MTs' => row['failed_mt'] || 0,
+               'Average Transaction Time' => row['avg_transaction_time'],
+               'Average Aggregator Time' => row['avg_aggregator_time'])
+      end
     end
+
+    remember(:last_run, Time.now)
   end
 end
