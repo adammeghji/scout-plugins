574ff0609c28120fca08000ab35a4e54ebad8ace
Doug Barth <dougbarth@gmail.com>
2010-09-24 11:00:16 -0500

    Stop loading the rails environment.
    Loading the TMB environment results in the bundler locking us out of
    system gems, causing us problems. Therefore, we will read the YAML
    database file ourselves so we don't have to load the environment.

--- rails_session_monitor/rails_session_monitor.rb
+++ rails_session_monitor/rails_session_monitor.rb
@@ -1,40 +1,34 @@
 class RailsSessionMonitor < Scout::Plugin
-  TEST_USAGE = "#{File.basename($0)} days_for_old_sessions DAYS_FOR_OLD_SESSIONS path_to_app PATH_TO_APP rails_env RAILS_ENV"
+  OPTIONS=<<-EOS
+  path_to_app:
+    name: Full Path to the Rails Application
+    notes: "The full path to the Rails application (ex: /var/www/apps/APP_NAME/current)."
+  rails_env:
+    name: Rails environment that should be used
+    default: production
+  EOS
 
-  def run
-    ENV['RAILS_ENV'] = @options['rails_env']
-    # Ensure require options are provided
-    if @options['rails_env'].nil?
-      return { :error => {:subject => "Required option not provided",
-                          :body    => "Please specify the Rails environment (ie: production, development, test)."}}
-    elsif @options['path_to_app'].nil?
-      return { :error => {:subject => "Required option not provided",
-                          :body    => "The full path to your Rails application is required (ex: /var/www/apps/APP_NAME/current)."}}
-    elsif @options['days_for_old_sessions'].nil?
-      return { :error => {:subject => "Required option not provided",
-                          :body    => "The Days for Old Sessions option must be provided. Sessions older than this amount will be deleted."}}
+  needs 'active_record', 'action_controller', 'yaml', 'erb'
+
+  def build_report
+    app_path = option(:path_to_app)
+    
+    # Ensure path to db config provided
+    if !app_path or app_path.empty?
+      return error("The path to the Rails Application wasn't provided.","Please provide the full path to the Rails Application (ie - /var/www/apps/APP_NAME/current)")
     end
-    # Load the Rails Env
-    quietly { require "#{@options['path_to_app']}/config/environment" }
+    
+    db_config_path = app_path + '/config/database.yml'
+    
+    if !File.exist?(db_config_path)
+      return error("The database config file could not be found.", "The database config file could not be found at: #{db_config_path}. Please ensure the path to the Rails Application is correct.")
+    end
+    
+    db_config = YAML::load(ERB.new(File.read(db_config_path)).result)
+    ActiveRecord::Base.establish_connection(db_config[option(:rails_env)])
 
     total = ActiveRecord::SessionStore::Session.count
-    old = ActiveRecord::SessionStore::Session.count(:conditions => ["updated_at < ?", @options['days_for_old_sessions'].to_i.days.ago ])
     
-    {
-      :report => {
-        :total => total,
-        :old => old
-      }
-    }
-  rescue
-    { :error => {:subject => "Unable to Monitor Rails Sessions", 
-      :body => "The following exception was raised:\n\n#{$!.message}\n\n#{$!.backtrace}"}}
-  end
-
-  def quietly
-    old_verbose, $VERBOSE = $VERBOSE, false
-    yield
-  ensure
-    $VERBOSE = old_verbose
+    report(:total => total)
   end
 end
--- sms_sessions/sms_sessions.rb
+++ sms_sessions/sms_sessions.rb
@@ -1,35 +1,35 @@
 class SmsSessions < Scout::Plugin
-  TEST_USAGE = "#{File.basename($0)} path_to_app PATH_TO_APP rails_env RAILS_ENV"
+  OPTIONS=<<-EOS
+  path_to_app:
+    name: Full Path to the Rails Application
+    notes: "The full path to the Rails application (ex: /var/www/apps/APP_NAME/current)."
+  rails_env:
+    name: Rails environment that should be used
+    default: production
+  EOS
 
-  def run
-    ENV['RAILS_ENV'] = @options['rails_env']
-    # Ensure require options are provided
-    if @options['rails_env'].nil?
-      return { :error => {:subject => "Required option not provided",
-                          :body    => "Please specify the Rails environment (ie: production, development, test)."}}
-    elsif @options['path_to_app'].nil?
-      return { :error => {:subject => "Required option not provided",
-                          :body    => "The full path to your Rails application is required (ex: /var/www/apps/APP_NAME/current)."}}
+  needs 'active_record', 'yaml', 'erb'
+  require 'active_record'
+
+  def build_report
+    app_path = option(:path_to_app)
+    
+    # Ensure path to db config provided
+    if !app_path or app_path.empty?
+      return error("The path to the Rails Application wasn't provided.","Please provide the full path to the Rails Application (ie - /var/www/apps/APP_NAME/current)")
     end
-    # Load the Rails Env
-    quietly { require "#{@options['path_to_app']}/config/environment" }
+
+    db_config_path = app_path + '/config/database.yml'
+    
+    if !File.exist?(db_config_path)
+      return error("The database config file could not be found.", "The database config file could not be found at: #{db_config_path}. Please ensure the path to the Rails Application is correct.")
+    end
+    
+    db_config = YAML::load(ERB.new(File.read(db_config_path)).result)
+    ActiveRecord::Base.establish_connection(db_config[option(:rails_env)])
 
     total = ActiveRecord::Base.connection.select_value("SELECT count(*) from sms_sessions")
     
-    {
-      :report => {
-        :total => total
-      }
-    }
-  rescue
-    { :error => {:subject => "Unable to Monitor SMS Sessions", 
-      :body => "The following exception was raised:\n\n#{$!.message}\n\n#{$!.backtrace}"}}
-  end
-
-  def quietly
-    old_verbose, $VERBOSE = $VERBOSE, false
-    yield
-  ensure
-    $VERBOSE = old_verbose
+    report(:total => total)
   end
 end
