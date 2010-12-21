4afef620a9d6f4226d59c696ef738d15885bf42f
John Wood <john@signalhq.com>
2010-12-21 16:52:43 -0600

    Update http methods plugin to work with couchdb < 0.11

--- couchdb/couchdb_http_methods.rb
+++ couchdb/couchdb_http_methods.rb
@@ -15,13 +15,26 @@ class CouchDBHttpMethodsPlugin < Scout::Plugin
   needs 'net/http', 'json', 'facets'
 
   def build_report
-    base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/_stats/"
+    base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/"
 
-    stats = %w{count mean max stddev}
+    response = JSON.parse(Net::HTTP.get(URI.parse(base_url)))
+    version = response['version']
+
     http_methods = %w{GET POST PUT DELETE HEAD}
-    http_methods.each do |http_method|
-      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "httpd_request_methods/#{http_method}?range=#{option(:stats_range)}")))
-      stats.each { |stat| report("httpd_request_methods_#{http_method}_#{stat}".to_sym => response['httpd_request_methods'][http_method].ergo[stat] || 0) }
+    if version.to_f >= 0.11
+      stats = %w{count mean max stddev}
+      http_methods.each do |http_method|
+        response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/httpd_request_methods/#{http_method}?range=#{option(:stats_range)}")))
+        stats.each { |stat| report("httpd_request_methods_#{http_method}_#{stat}".to_sym => response['httpd_request_methods'][http_method].ergo[stat] || 0) }
+      end
+    else
+      http_methods.each do |http_method|
+        key = "requests_count_#{http_method}".to_sym
+        response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/httpd_request_methods/#{http_method}")))
+        count = response['httpd_request_methods'][http_method].ergo['count'] || 0
+        report(key => count - (memory(key) || 0))
+        remember(key, count)
+      end
     end
   end
 
