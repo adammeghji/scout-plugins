6fab256c0a409eed815355056bbf52dd16983b68
John Wood <john@signalhq.com>
2010-12-21 15:06:03 -0600

    Get 60 second interval counts for GET, POST, PUT, DELETE, and HEAD

--- couchdb/couchdb_http_methods.rb
+++ couchdb/couchdb_http_methods.rb
@@ -12,14 +12,12 @@ class CouchDBServerStatusPlugin < Scout::Plugin
   needs 'net/http', 'json', 'facets'
 
   def build_report
-    base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/"
+    base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/_stats/"
 
-    #stats = %w{count current min max stddev mean}
-    stats = %w{count current min max stddev mean}
-    response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats")))
-    #http_methods = %w{GET POST PUT DELETE HEAD COPY}
-    http_methods = %w{GET POST PUT}
+    stats = %w{count mean max stddev}
+    http_methods = %w{GET POST PUT DELETE HEAD}
     http_methods.each do |http_method|
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "httpd_request_methods/#{http_method}?range=60")))
       stats.each { |stat| report("httpd_request_methods_#{http_method}_#{stat}".to_sym => response['httpd_request_methods'][http_method].ergo[stat] || 0) }
     end
   end
