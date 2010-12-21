b7d0c673269fc5e93d3d439b66c580442df8d817
John Wood <john@signalhq.com>
2010-12-21 16:35:45 -0600

    CouchDB 0.10 and below don't support time range queries (well)

--- couchdb/couchdb_server_status.rb
+++ couchdb/couchdb_server_status.rb
@@ -8,7 +8,7 @@ class CouchDBServerStatusPlugin < Scout::Plugin
       label: The host that CouchDB is running on
       default: http://127.0.0.1
     stats_range:
-      label: The time range to fetch stats for in seconds (60, 300, or 900)
+      label: The time range to fetch stats for in seconds (60, 300, or 900).  Used for CouchDB 0.11 and higher.
       default: 300
   EOS
 
@@ -18,14 +18,22 @@ class CouchDBServerStatusPlugin < Scout::Plugin
     base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/"
 
     response = JSON.parse(Net::HTTP.get(URI.parse(base_url)))
-    report(:version => response['version'])
+    version = response['version']
+    report(:version => version)
 
-    stats = %w{count mean max stddev}
-    response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/httpd/requests?range=#{option(:stats_range)}")))
-    stats.each { |stat| report("requests_#{stat}".to_sym => response['httpd']['requests'].ergo[stat]) }
+    if version.to_f >= 0.11
+      stats = %w{count mean max stddev}
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/httpd/requests?range=#{option(:stats_range)}")))
+      stats.each { |stat| report("requests_#{stat}".to_sym => response['httpd']['requests'].ergo[stat]) }
 
-    response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/request_time?range=#{option(:stats_range)}")))
-    stats.each { |stat| report("request_time_#{stat}".to_sym => response['couchdb']['request_time'][stat]) }
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/request_time?range=#{option(:stats_range)}")))
+      stats.each { |stat| report("request_time_#{stat}".to_sym => response['couchdb']['request_time'][stat]) }
+    else
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/httpd/requests")))
+      requests_count = response['httpd']['requests'].ergo['count'] || 0
+      report(:requests_count => requests_count - (memory(:requests_count) || 0))
+      remember(:requests_count, requests_count)
+    end
   end
 
 end
