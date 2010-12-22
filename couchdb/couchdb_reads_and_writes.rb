f0de430b7e084b28532fea38d9e440f16a477386
John Wood <john@signalhq.com>
2010-12-22 07:25:25 -0600

    Add plugin to track CouchDB reads and writes

---
+++ couchdb/couchdb_reads_and_writes.rb
@@ -0,0 +1,45 @@
+class CouchDBReadsAndWritesPlugin < Scout::Plugin
+
+  OPTIONS = <<-EOS
+    couchdb_port:
+      label: The port that CouchDB is running on
+      default: 5984
+    couchdb_host:
+      label: The host that CouchDB is running on
+      default: http://127.0.0.1
+    stats_range:
+      label: The time range to fetch stats for in seconds (60, 300, or 900)
+      default: 300
+  EOS
+
+  needs 'net/http', 'json', 'facets'
+
+  def build_report
+    base_url = "#{option(:couchdb_host)}:#{option(:couchdb_port)}/"
+
+    response = JSON.parse(Net::HTTP.get(URI.parse(base_url)))
+    version = response['version']
+
+    if version.to_f >= 0.11
+      stats = %w{sum mean max stddev}
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/database_reads?range=#{option(:stats_range)}")))
+      stats.each { |stat| report("database_reads_#{stat}".to_sym => response['couchdb']['database_reads'].ergo[stat] || 0) }
+
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/database_writes?range=#{option(:stats_range)}")))
+      stats.each { |stat| report("database_writes_#{stat}".to_sym => response['couchdb']['database_writes'].ergo[stat] || 0) }
+    else
+      key = "database_reads_sum".to_sym
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/database_reads")))
+      count = response['couchdb']['database_reads'].ergo['current'] || 0
+      report(key => count - (memory(key) || 0))
+      remember(key, count)
+
+      key = "database_writes_sum".to_sym
+      response = JSON.parse(Net::HTTP.get(URI.parse(base_url + "_stats/couchdb/database_writes")))
+      count = response['couchdb']['database_writes'].ergo['current'] || 0
+      report(key => count - (memory(key) || 0))
+      remember(key, count)
+    end
+  end
+
+end
